79e4eafa13af678db350688e5de40b40aa8de8ab
Bacquet, Sebastien <Sebastien.Bacquet@finastra.com>
2019-10-02 10:40:38 +0200

    Better Snapshot PDB test

--- Infrastructure.Tests/Oracle.fs
+++ Infrastructure.Tests/Oracle.fs
@@ -29,12 +29,20 @@ let ``Snapshot PDB`` () =
     let res = result {
         let! _ = oracleAPI.ImportPDB "/u01/app/oracle/oradata/SB_PDBs/test1.xml" "/u01/app/oracle/oradata/SB_PDBs" "source" |> Async.RunSynchronously
         let! _ = oracleAPI.SnapshotPDB "source" "/u01/app/oracle/oradata/SB_PDBs" "snapshot" |> Async.RunSynchronously
-        return! oracleAPI.PDBHasSnapshots "source" |> Async.RunSynchronously
+        let! hasSnapshots = oracleAPI.PDBHasSnapshots "source" |> Async.RunSynchronously
+        return! if hasSnapshots then Ok "Snapshot" else Error (exn "No snapshot ??!!")
     }
     res |> Result.mapError raise |> ignore
+
     let res = result {
         let! _ = oracleAPI.DeletePDB "snapshot" |> Async.RunSynchronously
+        let! stillExists = oracleAPI.PDBExists "snapshot" |> Async.RunSynchronously
+        let! _ = if stillExists then Error (exn "Snapshot not deleted ??!!") else Ok "Snapshot deleted"
+        let! hasSnapshots = oracleAPI.PDBHasSnapshots "source" |> Async.RunSynchronously
+        let! _ = if hasSnapshots then Error (exn "Yet some snapshot ??!!") else Ok "No snapshot"
         let! _ = oracleAPI.DeletePDB "source" |> Async.RunSynchronously
-        return! oracleAPI.PDBHasSnapshots "source" |> Async.RunSynchronously
+        let! stillExists = oracleAPI.PDBExists "source" |> Async.RunSynchronously
+        let! _ = if stillExists then Error (exn "Source not deleted ??!!") else Ok "Source deleted"
+        return "Everything fine!"
     }
     res |> Result.mapError raise |> ignore
