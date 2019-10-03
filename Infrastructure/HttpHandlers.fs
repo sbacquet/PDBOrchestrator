a74e00e3a635c6672355d5388edf61f1e0e68f98
Bacquet, Sebastien <Sebastien.Bacquet@finastra.com>
2019-10-03 18:31:35 +0200

    Rest API bootstrap

---
+++ Infrastructure/HttpHandlers.fs
@@ -0,0 +1,12 @@
+﻿module Infrastructure.HttpHandlers
+
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Logging
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open Giraffe
+
+let handleGetHello next (ctx:HttpContext) = task {
+    //let! body = ctx.ReadBodyFromRequestAsync()
+    ctx.GetLogger().Log(LogLevel.Information, "Hello request received with trace={Trace}", ctx.TraceIdentifier)
+    return! text "hello" next ctx
+}
--- Infrastructure/Program.fs
+++ Infrastructure/Program.fs
@@ -1,4 +1,6 @@
-﻿open Infrastructure
+﻿module PDBOrchestrator
+
+open Infrastructure
 open Application.Common
 open Microsoft.Extensions.Logging
 open Serilog.Extensions.Logging
@@ -9,33 +11,79 @@ open Serilog
 open Akka.Actor
 open Akkling
 open Domain.Common.Validation
+open System
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Options
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Configuration
+open Giraffe
+
+let loggerFactory = new SerilogLoggerFactory(dispose=true) :> ILoggerFactory
+
+[<RequireQualifiedAccess>]
+module Rest =
+    let webApp : HttpFunc -> HttpFunc = 
+        choose [
+            subRoute "/api"
+                (choose [
+                    GET >=> choose [
+                        route "/hello" >=> HttpHandlers.handleGetHello
+                    ]
+                ])
+            setStatusCode 404 >=> text "Not found" 
+        ]
+
+    let errorHandler (ex : Exception) (logger : Microsoft.Extensions.Logging.ILogger) =
+        logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
+        clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+    let configureApp (app : IApplicationBuilder) =
+        let env = app.ApplicationServices.GetService<IHostingEnvironment>()
+        (match env.IsDevelopment() with
+        | true  -> app.UseDeveloperExceptionPage()
+        | false -> app.UseGiraffeErrorHandler errorHandler)
+            //.UseHttpsRedirection()
+            //.UseCors(configureCors)
+            //.UseAuthentication()
+            .UseGiraffe(webApp) |> ignore
+
+    let configureServices (config : IConfiguration) (services : IServiceCollection) =
+        services
+            .AddSingleton(typeof<ILoggerFactory>, loggerFactory)
+            .AddGiraffe() |> ignore
+
+    let buildConfiguration (args:string[]) =
+        ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build()
 
 [<EntryPoint>]
 let main args =
     Serilog.Log.Logger <- LoggerConfiguration().WriteTo.Console().CreateLogger()
-    let loggerFactory = new SerilogLoggerFactory(dispose=true) :> ILoggerFactory
     let akkaConfig = 
         Akkling.Configuration.parse @"
     akka { 
         loggers=[""Akka.Logger.Serilog.SerilogLogger, Akka.Logger.Serilog""] 
     }"
 
-    let rootFolder = System.Environment.CurrentDirectory
-    let orchestratorName = "orchestrator"
-    let orchestratorPath = sprintf "%s\%s" rootFolder orchestratorName
-    let orchestratorRepo = OrchestratorRepository.OrchestratorRepository orchestratorPath :> IOrchestratorRepository
-    let oracleInstanceRepo = OracleInstanceRepository.OracleInstanceRepository orchestratorPath
-    let getMasterPDBRepo (instance:OracleInstance) = 
-        MasterPDBRepository.loadMasterPDBRepository (OracleInstanceRepository.instanceFolder orchestratorPath instance.Name) instance.MasterPDBs
-    let getOracleAPI (instance:OracleInstance) = Oracle.OracleAPI(loggerFactory, Oracle.connAsDBAFromInstance instance, Oracle.connAsDBAInFromInstance instance)
-    let orchestrator = orchestratorRepo.Get orchestratorName
+    //let rootFolder = System.Environment.CurrentDirectory
+    //let orchestratorName = "orchestrator"
+    //let orchestratorPath = sprintf "%s\%s" rootFolder orchestratorName
+    //let orchestratorRepo = OrchestratorRepository.OrchestratorRepository orchestratorPath :> IOrchestratorRepository
+    //let oracleInstanceRepo = OracleInstanceRepository.OracleInstanceRepository orchestratorPath
+    //let getMasterPDBRepo (instance:OracleInstance) = 
+    //    MasterPDBRepository.loadMasterPDBRepository (OracleInstanceRepository.instanceFolder orchestratorPath instance.Name) instance.MasterPDBs
+    //let getOracleAPI (instance:OracleInstance) = Oracle.OracleAPI(loggerFactory, Oracle.connAsDBAFromInstance instance, Oracle.connAsDBAInFromInstance instance)
+    //let orchestrator = orchestratorRepo.Get orchestratorName
 
-    use system = Akkling.System.create "pdb-orchestrator-system" akkaConfig
-    let orchestratorActor = system |> OrchestratorActor.spawn getOracleAPI oracleInstanceRepo getMasterPDBRepo orchestrator
-    let ctx = API.consAPIContext system orchestratorActor loggerFactory
+    //use system = Akkling.System.create "pdb-orchestrator-system" akkaConfig
+    //let orchestratorActor = system |> OrchestratorActor.spawn getOracleAPI oracleInstanceRepo getMasterPDBRepo orchestrator
+    //let ctx = API.consAPIContext system orchestratorActor loggerFactory
 
-    System.Console.WriteLine "Ready"
-
+#if false
     // TEST --------------------------------------------------------
     let state = API.getState ctx
     printfn "State:\n%A" state
@@ -49,11 +97,22 @@ let main args =
         System.Threading.Thread.Sleep 5000
         printfn "Request for %s = %A" (req.ToString()) (API.getRequestStatus ctx req |> run)
     | Invalid errors -> printfn "Cannot snapshot : %A" errors
-    // TEST --------------------------------------------------------
 
     System.Console.ReadKey() |> ignore
+    // TEST --------------------------------------------------------
+#endif
+
+    let config = Rest.buildConfiguration args
+    WebHostBuilder()
+        .UseConfiguration(config)
+        .UseKestrel()
+        .UseIISIntegration()
+        .Configure(Action<IApplicationBuilder> Rest.configureApp)
+        .ConfigureServices(Rest.configureServices config)
+        .Build()
+        .Run()
 
     System.Console.WriteLine "Exiting..."
-    system.Stop(untyped orchestratorActor)
-    system.Terminate().Wait()
+    //system.Stop(untyped orchestratorActor)
+    //system.Terminate().Wait()
     0
