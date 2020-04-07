1439ddabca3f19e7eb7b02f1f7826e8c65f18d61
Bacquet, Sebastien <Sebastien.Bacquet@finastra.com>
2020-04-07 16:29:58 +0200

    Fixed runProcessAsync when executable not found

--- Infrastructure.Tests/RunProcessAsync.fs
+++ Infrastructure.Tests/RunProcessAsync.fs
@@ -6,7 +6,9 @@ open Xunit
 
 [<Fact>]
 let ``Run process async`` () =
-    let x = runProcessAsync None "impdp" [ "help=y" ] |> Async.RunSynchronously
+    let x = runProcessAsync None "ipconfig" [ "/all" ] |> Async.RunSynchronously
     Assert.Equal(Ok 0, x)
-    let x = runProcessAsync None "impdp" [ "toto/toto@toto" ] |> Async.RunSynchronously
+    let x = runProcessAsync None "ipconfig" [ "/zzz" ] |> Async.RunSynchronously
     Assert.Equal(Ok 1, x)
+    let x = runProcessAsync None "zzz" [] |> Async.RunSynchronously
+    Assert.True(match x with | Error _ -> true | Ok _ -> false)
